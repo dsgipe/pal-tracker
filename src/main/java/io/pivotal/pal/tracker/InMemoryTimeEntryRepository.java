0d194e8595209fd76948d206afca689b91f437d9
steve <dsgipe@gmail.com>
2018-07-10 13:53:37 -0600

    WIP in memory tests pass

---
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -0,0 +1,47 @@
+package io.pivotal.pal.tracker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InMemoryTimeEntryRepository implements  TimeEntryRepository {
+
+    Map<Long, TimeEntry> timeEntrys;
+
+
+    public InMemoryTimeEntryRepository() {
+        timeEntrys = new HashMap<Long,TimeEntry>();
+    }
+
+    public TimeEntry create(TimeEntry timeEntry) {
+
+        if(find(timeEntry.getId()) != null) {
+            timeEntry.setId(timeEntry.getId()+1);
+            timeEntrys.put(timeEntry.getId(), timeEntry);
+        }else{
+            timeEntrys.put(timeEntry.getId(), timeEntry);
+        }
+
+        return timeEntry;
+    }
+    public TimeEntry find(long id){
+
+        return timeEntrys.get(id);
+    }
+
+    public List<TimeEntry> list() {
+        return  new ArrayList<TimeEntry>(timeEntrys.values());
+    }
+
+    public TimeEntry update(long id, TimeEntry timeEntry) {
+        timeEntry.setId(id);
+        timeEntrys.put(timeEntry.getId(), timeEntry);
+
+        return find(id);
+    }
+
+    public void delete(long id) {
+        timeEntrys.remove(id);
+    }
+}
---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -0,0 +1,41 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public  interface TimeEntryRepository {
+    public TimeEntry create(TimeEntry timeEntry) ;
+    public TimeEntry find(long id);
+
+    public List<TimeEntry> list();
+
+    public TimeEntry update(long id, TimeEntry timeEntry);
+
+    public void delete(long id);
+}
+/*    @Override
+    public ResponseEntity create(TimeEntry any) {
+        return  null;
+    }
+
+    @Override
+    public void find(long l) {
+
+    }
+
+    @Override
+    public ResponseEntity update(long eq, TimeEntry any) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<TimeEntry> delete(long l) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<TimeEntry>> list() {
+        return null;
+    }
+    */
